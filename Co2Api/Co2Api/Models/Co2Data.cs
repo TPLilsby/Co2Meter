c5570be6baa39fa614cdcdf091acd225699865dc
Tobias Philip Lilsby (TOBI322B) <tobi322b@zbc.dk>
2025-06-02 13:19:26 +0200

    test

---
+++ Co2Api/Co2Api/Models/Co2Data.cs
@@ -0,0 +1,9 @@
+﻿namespace Co2Api.Models
+{
+    public class Co2Data
+    {
+        public int Id { get; set; } 
+        public DateTime Timestamp { get; set; }
+        public double Value { get; set; }
+    }
+}
